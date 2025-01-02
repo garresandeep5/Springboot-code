1b6171edf951353e9725053252437d7615aef21c
GARRE SANDEEP <100399146+garresandeep5@users.noreply.github.com>
2025-01-02 18:33:48 +0530

    Delete scripits/start.sh

--- scripits/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting ECS service..."
-# Example: Force new deployment in ECS
-aws ecs update-service --cluster my-cluster --service my_service --force-new-deployment
-echo "Application started."
-exit 0
