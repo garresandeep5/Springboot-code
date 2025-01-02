25b3cefd0d3db08bfdee687adc85097b82e91574
GARRE SANDEEP <100399146+garresandeep5@users.noreply.github.com>
2025-01-02 18:34:02 +0530

    Delete scripits/validate.sh

--- scripits/validate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Validating environment..."
-# Example: Check if environment variables are set
-if [ -z "$ENV_VAR_NAME" ]; then
-  echo "Error: ENV_VAR_NAME is not set."
-  exit 1
-fi
-echo "Environment validation passed."
-exit 0
